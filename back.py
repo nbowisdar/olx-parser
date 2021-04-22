e03c853be8519315406dd8ff095ac452969b7919
nbowisdar <78906490+nbowisdar@users.noreply.github.com>
2021-04-22 19:00:09 +0300

    First good parsing

---
+++ back.py
@@ -0,0 +1,20 @@
+url = 'https://www.olx.ua/hobbi-otdyh-i-sport/antikvariat-kollektsii/dnp/q-монеты/'
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+agent = UserAgent().random
+headers = {
+    "Accept": "*/*",
+    "User-Agent": agent
+}
+s = requests.get(url, headers)
+data = {}
+soup = BeautifulSoup(s.text,'lxml')
+
+info = soup.find(class_='fixed offers breakword redesigned').find_all(class_='lheight22 margintop5')
+links = [i.find('a').get('href') for i in info]
+prices = [i.text.replace('\n','') for i in soup.find(class_='fixed offers breakword redesigned').find_all('div',class_='space inlblk rel')]
+for i in range(len(prices)):
+    data[links[i]] = prices[i]
+# for link, price in data.items():
+#     print(link, price)
